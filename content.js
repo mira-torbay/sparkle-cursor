9415d79a1980cdcf3df80407356ea7a50bc55c30
Mira Torbay <118401965+mira-torbay@users.noreply.github.com>
2025-02-15 21:53:19 -0500

    OK *now I fixed the shadow

--- content.js
+++ content.js
@@ -15,7 +15,7 @@ document.addEventListener('mousemove', (event) => {
     // set css vars
     sparkle.style.setProperty('--sparkle-colour1', lightenColour(sparkleColour, 40));
     sparkle.style.setProperty('--sparkle-colour2', sparkleColour);
-    sparkle.style.setProperty('--sparkle-shadow', sparkleColour);
+    sparkle.style.setProperty('--sparkle-shadow', lightenColour(sparkleColour, 20));
 
     sparkle.style.left = `${event.pageX}px`;
     sparkle.style.top = `${event.pageY}px`;
